212242c32da2dd4fd2e674f6d4fea6a5c554d1e1
aymfx <1095731371@qq.com>
2020-04-28 13:42:27 +0800

    aymfx -

--- demos/index.js
+++ demos/index.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-debugger
 const Koa = require('../lib/application.js');
 const app = new Koa();
-
+// console.log(app, 1212);s
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(3000);
+app.listen(3001);
