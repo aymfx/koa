223e430e23ef9057cb26fdd85968c047124815fe
aymfx <1095731371@qq.com>
2020-04-30 13:41:31 +0800

    aymfx - 安排

--- demos/index.js
+++ demos/index.js
@@ -2,9 +2,23 @@
 const Koa = require('../lib/application.js');
 const app = new Koa();
 // console.log(app, 1212);s
+
 app.use(async ctx => {
-  ctx.cookies.set('name', 'tobi', { signed: true });
-  ctx.body = 'Hello World';
+  // ctx.cookies = '1995';
+  // throw new Error('xxx');
+  console.log(ctx);
+  ctx.status = 200;
+  ctx.set('ETag', '129');
+  // 缓存是好的
+  if (ctx.fresh) {
+    ctx.status = 304;
+    return;
+  }
+  ctx.body = 'wod20是是是';
 });
 
+// app.on('error', err => {
+//   // console.error('server error', err);
+// });
 app.listen(3001);
+app.context.name = 'ly';
